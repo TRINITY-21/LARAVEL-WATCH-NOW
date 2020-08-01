2f2cba757bc21d8cacdc5f5645a55698ee677078
= <=>
2020-07-31 21:58:01 -0700

    Debug

--- app/Http/Controllers/MoviesController.php
+++ app/Http/Controllers/MoviesController.php
@@ -18,14 +18,14 @@ class MoviesController extends Controller
     {
         $popularMovies = Http::withToken(config('services.tmdb.token'))
             ->get('https://api.themoviedb.org/3/movie/popular')
-            ->json();
+            ->json()['results'];
         $nowPlayingMovies = Http::withToken(config('services.tmdb.token'))
             ->get('https://api.themoviedb.org/3/movie/now_playing')
-            ->json();
+            ->json()['results'];
 
         $genres = Http::withToken(config('services.tmdb.token'))
             ->get('https://api.themoviedb.org/3/genre/movie/list')
-            ->json();
+            ->json()['genres'];
 
         $viewModel = new MoviesViewModel(
             $popularMovies,
