7d5ac22459bce9127247b89ff6e55061dd38f58b
= <=>
2020-08-01 09:23:25 -0700

    initial commit

--- resources/views/movies/show.blade.php
+++ resources/views/movies/show.blade.php
@@ -33,7 +33,7 @@
                 <div x-data="{ isOpen: false }">
                 @if(count($movie['videos']['results']) > 0)
                             <div class="mt-12">
-                                <button
+                                <button 
                                     @click="isOpen = true"
                                     class="flex inline-flex items-center bg-orange-500 text-gray-900 rounded font-semibold px-5 py-4 hover:bg-orange-600 transition ease-in-out duration-150">
                                     <svg class="w-6 fill-current" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"/><path d="M10 16.5l6-4.5-6-4.5v9zM12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z"/></svg>
@@ -86,7 +86,7 @@
                     <div class="mt-2">
                         <a href="{{ route('actors.show', $cast['id']) }}" class="text-lg mt-2 hover:text-gray-300"> {{ $cast['name'] }} </a>
                         <div class="text-gray-400 text-sm">
-                            {{ $cast['character'] }}
+                            {{ $cast['character'] }} 
                         </div>
                     </div>
                 </div>
@@ -99,11 +99,11 @@
             <h2 class="text-4xl font-semibold">Images</h2>
             <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8 mt-8">
             @foreach ($movie['images'] as $image)
-                    <a
+                    <a 
                     @click.prevent="
                     isOpen = true
                     image='{{ 'https://image.tmdb.org/t/p/original'.$image['file_path'] }}'
-                    "
+                    "                            
                     href="#">
                         <img src="{{ 'https://image.tmdb.org/t/p/w500/'.$image['file_path'] }}" alt="" class="hover:opacity-75 transition ease-in-out duration-150">
                     </a>
@@ -131,6 +131,4 @@
             </div>
         </div>
     </div> <!-- end movie-images -->
-
-
 @endsection
